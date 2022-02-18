3a9b79216f4303f9c9af3b0cb17e3bc78238809a
junsung.yoon <junsung.yoon@devsisters.com>
2022-02-18 18:30:40 +0900

    더미 글들 삭제

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `Junbyeol Blog`,
+  description: ``,
+  author: `Junsung Yoon`,
+  introduction: `안녕하세요`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
